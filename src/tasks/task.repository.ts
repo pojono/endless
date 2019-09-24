9a80bcf98b12fd8eedacfa167164c7ea6b2d7df9
pojono <pojono@gmail.com>
2019-09-24 15:48:37 +0300

    add search

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -13,11 +13,11 @@ export class TaskRepository extends Repository<Task> {
     const query = this.createQueryBuilder('task');
 
     if (status) {
-      query.andWhere('task.status = :status', { status })
+      query.andWhere('task.status = :status', { status });
     }
 
     if (search) {
-
+      query.andWhere('(task.title LIKE :search OR task.description LIKE :search)', { search: `%${search}%` });
     }
 
     const tasks = await query.getMany();
